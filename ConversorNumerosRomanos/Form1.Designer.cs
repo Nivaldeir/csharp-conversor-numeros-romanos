9b3d527df05a47c006d7621af85c740184d355ee
Nivaldeir <nivaldeir-silva@hotmail.com>
2021-08-11 22:37:01 -0300

    Algoritmo que converte algarismo em numeros Romanos (de 1 até 1 Milhão), e converter numeros Romanos em algarismo (de 1 até 3000)

---
+++ ConversorNumerosRomanos/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace ConversorNumerosRomanos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConverter = new Guna.UI2.WinForms.Guna2Button();
+            this.txtNumero = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.CheckedState.Parent = this.btnConverter;
+            this.btnConverter.CustomImages.Parent = this.btnConverter;
+            this.btnConverter.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnConverter.ForeColor = System.Drawing.Color.White;
+            this.btnConverter.HoverState.Parent = this.btnConverter;
+            this.btnConverter.Location = new System.Drawing.Point(113, 158);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.ShadowDecoration.Parent = this.btnConverter;
+            this.btnConverter.Size = new System.Drawing.Size(126, 36);
+            this.btnConverter.TabIndex = 0;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNumero.DefaultText = "";
+            this.txtNumero.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtNumero.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtNumero.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtNumero.DisabledState.Parent = this.txtNumero;
+            this.txtNumero.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtNumero.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtNumero.FocusedState.Parent = this.txtNumero;
+            this.txtNumero.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtNumero.HoverState.Parent = this.txtNumero;
+            this.txtNumero.Location = new System.Drawing.Point(74, 101);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.PasswordChar = '\0';
+            this.txtNumero.PlaceholderText = "";
+            this.txtNumero.SelectedText = "";
+            this.txtNumero.ShadowDecoration.Parent = this.txtNumero;
+            this.txtNumero.Size = new System.Drawing.Size(200, 36);
+            this.txtNumero.TabIndex = 1;
+            this.txtNumero.Click += new System.EventHandler(this.txtNumero_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(160, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(160, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Numero de 1 até 1 milhão";
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.FocusedState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.HoverState.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Location = new System.Drawing.Point(506, 101);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PasswordChar = '\0';
+            this.guna2TextBox1.PlaceholderText = "";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.ShadowDecoration.Parent = this.guna2TextBox1;
+            this.guna2TextBox1.Size = new System.Drawing.Size(200, 36);
+            this.guna2TextBox1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(542, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Numero de 1 até 3000";
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(544, 158);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(126, 36);
+            this.guna2Button1.TabIndex = 4;
+            this.guna2Button1.Text = "Converter";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(604, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(813, 301);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.btnConverter);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button btnConverter;
+        private Guna.UI2.WinForms.Guna2TextBox txtNumero;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private System.Windows.Forms.Label label4;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ConversorNumerosRomanos/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversorNumerosRomanos
+{
+    public partial class Form1 : Form
+    {
+        String nr = String.Empty;
+        int digitoRomanoemconjunto = 0;
+        int teste;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string romana(double n)
+        {
+            nr = "";
+            int unidades, centenas, dezenas, Unidade_unidade, Unidade_Dezenas, Unidade_Centenas, Unidade_Milhao; //criação das variaveis
+
+            Unidade_Milhao = (int)(n / 1000000);
+            n = n - Unidade_Milhao * 1000000;
+
+            Unidade_Centenas = (int)(n / 100000);
+            n = n - Unidade_Centenas * 100000;
+
+            Unidade_Dezenas = (int)(n / 10000);
+            n = n - Unidade_Dezenas * 10000;
+
+            Unidade_unidade = (int)(n / 1000);
+            n = n - Unidade_unidade * 1000;
+
+            centenas = (int)(n / 100);
+            n = n - centenas * 100; // calcular e tirar centenas do numero
+            dezenas = (int)n / 10;
+
+            n = n - dezenas * 10;//calcuar e tirar dezenas do numero
+            unidades = Convert.ToInt32(n);//unidades
+
+            switch (Unidade_Milhao)
+            {
+                case 1:
+                    label6.Text = "_";
+                    nr = "M";
+                    break;
+            } // Unidade de Milhao
+            switch (Unidade_Centenas)
+            {
+                case 1:
+                    label6.Text = label6.Text + "_";
+                    nr = nr + "C";
+                    break;
+                case 2:
+                    label6.Text = label6.Text + "__";
+                    nr = nr + "CC";
+                    break;
+                case 3:
+                    label6.Text = label6.Text + "___";
+                    nr = nr + "CCC";
+                    break;
+                case 4:
+                    label6.Text = label6.Text + "__";
+                    nr = nr + "CD";
+                    break;
+                case 5:
+                    label6.Text = label6.Text + "_";
+                    nr = nr + "D";
+                    break;
+                case 6:
+                    label6.Text = label6.Text + "__";
+                    nr = nr + "DC";
+                    break;
+                case 7:
+                    label6.Text = label6.Text + "___";
+                    nr = nr + "DCC";
+                    break;
+                case 8:
+                    label6.Text = label6.Text + "____";
+                    nr = nr + "DCCC";
+                    break;
+                case 9:
+                    label6.Text = label6.Text + "__";
+                    nr = nr + "CM";
+                    break;
+            } // Centenas de milhar
+
+            switch (Unidade_Dezenas)
+            {
+                case 1:
+                    label6.Text = label6.Text + "_";
+                    nr = nr + "X";
+                    break;
+                case 2:
+                    label6.Text = label6.Text + "__";
+                    nr = nr + "XX";
+                    break;
+                case 3:
+                    label6.Text = label6.Text + "___";
+                    nr = nr + "XXX";
+                    break;
+                case 4:
+                    label6.Text = label6.Text + "__";
+                    nr = nr + "XL";
+                    break;
+                case 5:
+                    label6.Text = label6.Text + "_";
+                    nr = nr + "L";
+                    break;
+                case 6:
+                    label6.Text = label6.Text + "__";
+                    nr = nr + "LX";
+                    break;
+                case 7:
+                    label6.Text = label6.Text + "___";
+                    nr = nr + "LXX";
+                    break;
+                case 8:
+                    label6.Text = label6.Text + "____";
+                    nr = nr + "LXXX";
+                    break;
+                case 9:
+                    label6.Text = label6.Text + "__";
+                    nr = nr + "XC";
+                    break;
+            } // Dezenas de milhar
+
+            switch (Unidade_unidade)
+            {
+                case 1: nr = nr + "M"; break;
+                case 2: nr = nr + "MM"; break;
+                case 3: nr = nr + "MMM"; break;
+                case 4:
+                    label6.Text = label6.Text + "__";
+                    nr = "IV";
+                    break;
+                case 5:
+                    label6.Text = label6.Text + "_";
+                    nr = nr + "V";
+                    break;
+                case 6:
+                    label6.Text = label6.Text + "__";
+                    nr = nr + "VI";
+                    break;
+                case 7:
+                    label6.Text = label6.Text + "_";
+                    nr = nr + "VII";
+                    break;
+                case 8:
+                    label6.Text = label6.Text + "_";
+                    nr = nr + "VIII";
+                    break;
+                case 9:
+                    label6.Text = label6.Text + "_";
+                    nr = nr + "IX";
+                    break;
+            } //Unidade_Dezenas de milhar
+
+            switch (centenas)
+            {
+                case 1: nr = nr + "C"; break;
+                case 2: nr = nr + "CC"; break;
+                case 3: nr = nr + "CCC"; break;
+                case 4: nr = nr + "CD"; break;
+                case 5: nr = nr + "D"; break;
+                case 6: nr = nr + "DC"; break;
+                case 7: nr = nr + "DCC"; break;
+                case 8: nr = nr + "DCCC"; break;
+                case 9: nr = nr + "CM"; break;
+                case 10: nr = nr + "M"; break;
+                case 11: nr = nr + "MM"; break;
+                case 12: nr = nr + "MMM"; break;
+            } // Centena
+
+            switch (dezenas)
+            {
+                case 1: nr = nr + "X"; break;
+                case 2: nr = nr + "XX"; break;
+                case 3: nr = nr + "XXX"; break;
+                case 4: nr = nr + "XL"; break;
+                case 5: nr = nr + "L"; break;
+                case 6: nr = nr + "LX"; break;
+                case 7: nr = nr + "LXX"; break;
+                case 8: nr = nr + "LXXX"; break;
+                case 9: nr = nr + "XC"; break;
+            } // Dezenas
+
+            switch (unidades)
+            {
+                case 1: nr = nr + "I"; break;
+                case 2: nr = nr + "II"; break;
+                case 3: nr = nr + "III"; break;
+                case 4: nr = nr + "IV"; break;
+                case 5: nr = nr + "V"; break;
+                case 6: nr = nr + "VI"; break;
+                case 7: nr = nr + "VII"; break;
+                case 8: nr = nr + "VIII"; break;
+                case 9: nr = nr + "IX"; break;
+            } // Unidades
+
+            return nr;
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                romana(double.Parse(txtNumero.Text));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Dados invalidos, por favor inserir apenas numeros");
+                txtNumero.Clear();
+                label5.Text = "";
+                label6.Text = "";
+            }
+            label5.Text = nr.ToString();
+        }
+
+        public void verificarNumerosRomanos(string numeroRomano)
+        {
+            int soma = 0;
+            if (guna2TextBox1.Text == "CCCC" || guna2TextBox1.Text == "MMMM" || guna2TextBox1.Text == "MMMMM")
+            {
+                MessageBox.Show("Numero romano invalido");
+                return;
+            }
+            int NumeroRomanoTest = 0;
+            for (int i = 0; i < numeroRomano.Length; i++)
+            {
+                switch (numeroRomano[i])
+                {
+                    case 'I':
+                        NumeroRomanoTest += 1;
+                        break;
+                    case 'V':
+                        NumeroRomanoTest += 5;
+                        break;
+                    case 'X':
+                        NumeroRomanoTest += 10;
+                        break;
+                    case 'L':
+                        NumeroRomanoTest += 50;
+                        break;
+                    case 'C':
+                        NumeroRomanoTest += 100;
+                        break;
+                    case 'D':
+                        NumeroRomanoTest += 500;
+                        break;
+                    case 'M':
+                        NumeroRomanoTest += 1000;
+                        break;
+                }
+                char dr = numeroRomano[i];
+                char drSeguinte = i < numeroRomano.Length - 1 ? numeroRomano[i + 1] : ' ';
+
+                if (dr == 'I' && drSeguinte == 'V')
+                {
+                    digitoRomanoemconjunto += -2;
+                }
+
+                if (dr == 'I' && drSeguinte == 'X')
+                {
+                    digitoRomanoemconjunto += -2;
+                }
+
+                if (dr == 'X' && drSeguinte == 'L')
+                {
+                    digitoRomanoemconjunto += -20;
+                }
+
+                if (dr == 'X' && drSeguinte == 'C')
+                {
+                    digitoRomanoemconjunto += -20;
+
+                }
+
+                if (dr == 'C' && drSeguinte == 'D')
+                {
+                    digitoRomanoemconjunto += -200;
+                }
+
+                if (dr == 'C' && drSeguinte == 'M')
+                {
+                    digitoRomanoemconjunto += -200;
+                }
+            }
+            teste = NumeroRomanoTest;
+            soma = NumeroRomanoTest + digitoRomanoemconjunto;
+            label2.Text = soma.ToString();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+           verificarNumerosRomanos(guna2TextBox1.Text.ToString());
+        }
+
+        private void txtNumero_Click(object sender, EventArgs e)
+        {
+            txtNumero.Clear();
+            label6.Text = "";
+            label5.Text = "";
+        }
+    }
+}
